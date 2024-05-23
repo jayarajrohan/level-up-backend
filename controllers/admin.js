cc57d592861ee8e151757e56c5764be4d5f45144
jayarajrohan <iaminbest@gmail.com>
2024-05-23 13:28:32 +0530

    sameSite disabled

--- controllers/admin.js
+++ controllers/admin.js
@@ -51,7 +51,7 @@ exports.login = (req, res, next) => {
       res.cookie("token", token, {
         httpOnly: process.env.COOKIE_SETTINGS_HTTP_ONLY === "true",
         secure: process.env.COOKIE_SETTINGS_SECURE === "true",
-        sameSite: "none",
+        // sameSite: "none",
       });
 
       res.status(200).json({
